76c051886c695d33cbcdd3c5c4b6ee85d746238f
Maria Fernanda <nanamyrita@gmail.com>
2025-02-26 09:39:54 -0300

    Front_Alteracoes

---
+++ src/App2.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const App2 = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App2;
+
+//Arrow Function com refce
+
+// nomeação de componentes
+//PascalCase
+
+//quando eu declaro como default eu posso importar ele com qualquer nome no main.jsx
+
+//sem default so posso importar entre chaves com o proprio nome
+
+// Self closing tag
+// <Header> <Header>
+
+//Numeração de classes em CSS
+//Metodologia Bem
+//Block
+//Elements
+//Modifiers
+//bloco__elements--modificador
